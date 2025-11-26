b9b2f7290a88b8c05cd7912c45a2e497ecfb7ab5
LeoNasc1223 <leonardonascimento122312@gmail.com>
2025-11-26 22:08:05 +0000

    Alteração de código

--- app.py
+++ app.py
@@ -53,8 +53,6 @@ def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return "".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
-
-
 # --- Lógica de Automação Local ---
 ETRADE_PROCESS_NAME = "ETrade.exe"
 
@@ -3240,5 +3238,6 @@ if __name__ == '__main__':
             logger_app.error("Erro: 'ngrok.exe' ou 'cmd.exe' não foi encontrado. Certifique-se de que o ngrok está instalado e no seu PATH do sistema.")
         except Exception as e:
             logger_app.error(f"Erro ao tentar iniciar o ngrok: {e}")
+
 
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False) # use_reloader=False para evitar múltiplas sessões do ngrok
